069f4c40d18abc49104e6f612b3eeb4e8d6ba199
Old-Degen <125771254+Old-Degen@users.noreply.github.com>
2023-03-20 17:33:25 +0400

    Update wallet_generator.py

--- wallet_generator.py
+++ wallet_generator.py
@@ -1,95 +1,62 @@
 import tkinter as tk
-from tkinter import *
-from tkinter import ttk
-from wallet_generator import generate_wallets
-from wallet_manager import WalletManager
-import sys
-import os
+import csv
+from web3 import Web3, Account
 
 
-sys.path.append(os.path.abspath("/path/to/directory"))
+def generate_wallets(num_wallets_entry, group_name="", wallet_name=""):
+    try:
+        # Если не указано имя группы, создаем группу со стандартным именем
+        if not group_name:
+            group_name = "wallet_group"
+
+        # Если не указано имя кошельков, используем имя группы по умолчанию
+        if not wallet_name:
+            wallet_name = group_name
+
+        # Создаем Entry виджет для ввода количества кошельков
+        num_wallets_entry = tk.Entry(root, width=30)
+        num_wallets_entry.pack()
+
+        # Генерируем указанное количество кошельков
+        wallets = []
+        for i in range(num_wallets_entry):
+            account = Account.create()
+            name = f"{wallet_name}_{i+1}"
+            public_address = account.address
+            private_key = account.key.hex()
+
+            # Добавляем кошелек в список кошельков
+            wallets.append({"name": name, "address": public_address, "private_key": private_key})
+
+        # Создаем экземпляр класса WalletManager и добавляем кошельки
+        manager = WalletManager()
+        for wallet in wallets:
+            manager.add_wallet(wallet["name"], wallet["address"])
+
+    except Exception as e:
+        print(f"Error generating wallets: {e}")
 
 
-root = Tk()
-root.title("ToolBox")
+class WalletManager:
+    def __init__(self):
+        self.wallets = []
+        with open("wallets.csv") as f:
+            wallets_csv = csv.reader(f)
+            next(wallets_csv)  # skip header
+            for row in wallets_csv:
+                self.wallets.append(row)
 
-notebook = ttk.Notebook(root)
+    def add_wallet(self, name, address):
+        self.wallets.append({"name": name, "address": address})
+        self.save_wallets()
 
-# Создаем вкладку для создания кошельков
-tab1 = ttk.Frame(notebook)
-notebook.add(tab1, text="Create Wallets")
+    def delete_wallet(self, index):
+        del self.wallets[index]
+        self.save_wallets()
 
-# Добавляем элементы на вкладку создания кошельков
-group_label = Label(tab1, text="Group Name:")
-group_label.pack()
-group_name = Entry(tab1)
-group_name.pack()
-
-wallet_label = Label(tab1, text="Wallet Name:")
-wallet_label.pack()
-wallet_name = Entry(tab1)
-wallet_name.pack()
-
-num_label = Label(tab1, text="Number of Wallets:")
-num_label.pack()
-num_wallets_entry = Entry(tab1)
-num_wallets_entry.pack()
-
-
-def create_wallets():
-    num_wallets = int(num_wallets_entry.get())
-    manager = WalletManager()
-    for i in range(num_wallets):
-        wallet = manager.create_wallet()
-        manager.add_wallet(wallet.address, wallet.private_key)
-    manager.save_wallets()
-    create_wallets_window.destroy()
-
-
-
-def view_wallet():
-    # Создаем экземпляр класса WalletManager
-    manager = WalletManager()
-
-    # Получаем список кошельков из экземпляра класса
-    wallets = manager.get_wallets_list()
-
-    # Создаем список для отображения кошельков
-    wallet_listbox = Listbox(tab2)
-    wallet_listbox.pack()
-
-    # Очищаем список для обновления
-    wallet_listbox.delete(0, END)
-
-    # Добавляем каждый кошелек в список
-    for wallet in wallets:
-        wallet_listbox.insert(END, f"{wallet[0]} - {wallet[1]}: {wallet[2]}")
-
-def view_wallets():
-    manager = WalletManager()
-    if not manager.wallets:
-        messagebox.showinfo("Error", "No wallets found")
-        return
-    view_window = Toplevel(root)
-    view_window.title("View Wallets")
-    wallets_list = Listbox(view_window)
-    for wallet in manager.wallets:
-        wallets_list.insert(END, f"{wallet[0]} - {wallet[1]} - {wallet[2]} - {wallet[3]}")
-    wallets_list.pack()
-    view_window.mainloop()
-
-
-
-
-# Создаем кнопки для создания и просмотра кошельков
-generate_button = Button(tab1, text="Generate Wallets", command=create_wallets)
-generate_button.pack()
-
-view_button = Button(tab1, text="View Wallets", command=view_wallets)
-view_button.pack(side=LEFT, padx=10)
-
-
-
-notebook.pack()
-
-root.mainloop()
+    def save_wallets(self):
+        with open("wallets.csv", mode="w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(["name", "address"])
+            for wallet in self.wallets:
+                writer.writerow([wallet["name"], wallet["address"]])
